fd7cd9900a6e4e0f12e5139bb8bc54b43a8b53b1
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-04-09 10:18:18 -0600

    useReducer implemented to replace the custom hook 'useOrder'

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,9 +1,12 @@
+import { Dispatch } from "react";
+import { OrderActions } from "../reducers/order-reducer";
+
 type MenuItemProps = {
     item: MenuItem,
-    addItem: (item: MenuItem) => void
+    dispatch: Dispatch<OrderActions>
 }
 
-export default function MenuItem({ item, addItem }: MenuItemProps) {
+export default function MenuItem({ item, dispatch }: MenuItemProps) {
 
     const { name, price } = item;
 
@@ -11,7 +14,10 @@ export default function MenuItem({ item, addItem }: MenuItemProps) {
         <button
             type="button"
             className="border-2 border-teal-400 hover:bg-teal-200 transition w-full p-3 flex justify-between items-center"
-            onClick={() => addItem(item)}
+            onClick={() => dispatch({
+                type: 'add-item',
+                payload: { item }
+            })}
         >
             <p>{name}</p>
             <p className="font-black">${price}</p>
